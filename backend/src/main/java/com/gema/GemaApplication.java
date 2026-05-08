efd156e48ccd1a3b75ee4e7aaf7ffc5906adcd91
Gabriel Gerhardt <gabrielgerhardt27@gmail.com>
2026-05-08 14:54:12 -0300

    feat: creating frontend and backend structure GAB-5

---
+++ backend/src/main/java/com/gema/GemaApplication.java
@@ -0,0 +1,13 @@
+package com.gema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemaApplication.class, args);
+	}
+
+}
